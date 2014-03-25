b7a51a4e57f18f9c0794336b3a49dd9243019767
pipecaniza <pipecaniza@outlook.com>
2014-03-24 22:06:14 -0500

    Inicio de arquitectura

---
+++ PhysicalSimulator/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PhysicalSimulator
+{
+    class Entity : Object
+    {
+        private string tag;
+        private bool activeInScene;
+        private Animation animation;
+        private Collider collider;
+        private int layer;
+
+        public void SetActive(bool state)
+        {
+            this.activeInScene = state;
+        }
+
+        public bool CompareTag(string tag)
+        {
+            if(this.tag == tag)
+                return true;
+            return false;
+        }
+    }
+}
