2d70f3692c5886be8325a30ab8a34c9e25545997
Chamochaman <pipecaniza@gmail.com>
2014-05-27 19:42:29 -0500

    Form descripción
    al presionar f2 muestra la definición del tema y la bibliografía by José
    Santa

---
+++ PhysicalSimulator/Descripción.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PhysicalSimulator
+{
+    public partial class Descripción :Form
+
+    {
+        public Descripción()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// titulo y descripción del movimiento.
+        /// </summary>  
+        private Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+            
+                     
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Descripción));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descripción";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 90);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(360, 119);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Descripción
+            // 
+            this.ClientSize = new System.Drawing.Size(557, 284);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Descripción";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+    }
+}
+
